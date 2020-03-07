3d096066e683194d92ea0ae06188317ab9ba663a
7cc <4398901+7cc@users.noreply.github.com>
2020-03-08 01:30:19 +0900

    Update set_get_clipbpard.ps1

--- set_get_clipbpard.ps1
+++ set_get_clipbpard.ps1
@@ -1,9 +1,14 @@
 $PSVersionTable
+echo "run file set_get_clipbpard.ps1"
 
-1..2000 | % {
+1..10000 | % {
   Set-Clipboard $_
   $i = Get-Clipboard
   if ( $i -eq "" ) {
     echo $_
   }
 }
+
+Get-Clipboard
+
+echo "end file set_get_clipbpard.ps1"
