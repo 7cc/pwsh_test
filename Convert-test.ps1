6982fa7d9f4b76d3cb479ad2115683c0ac9e1122
7cc <4398901+7cc@users.noreply.github.com>
2020-04-11 19:01:15 +0900

    Create Convert-test.ps1

---
+++ Convert-test.ps1
@@ -0,0 +1,14 @@
+echo "Start Convert-test.ps1"
+$PSVersionTable
+
+$csv = @'
+"date"
+"2020-04-11T18:29:59+09:00"
+'@ | ConvertFrom-Csv
+
+$csv.date
+
+$json = '{ "date": "2020-04-11T18:29:59+09:00" }' | ConvertFrom-JSON
+$json.date
+
+echo "End Convert-test.ps1"
