996e980ce12dc64b1e532aa02d72454d22c67779
7cc <4398901+7cc@users.noreply.github.com>
2020-04-14 12:55:36 +0900

    Update Convert-test.ps1

--- Convert-test.ps1
+++ Convert-test.ps1
@@ -7,10 +7,10 @@ $obj.date # string
 $json =  $obj | ConvertTo-Json | ConvertFrom-Json
 $json.date.DateTime # datetime
 
-$csv = $j | ConvertTo-Csv | ConvertFrom-Csv
+$csv = $json | ConvertTo-Csv | ConvertFrom-Csv
 $csv.date # string
 
-$json = $c | ConvertTo-Json | ConvertFrom-Json
+$json = $csv | ConvertTo-Json | ConvertFrom-Json
 $json.date # string
 
 echo "End Convert-test.ps1"
